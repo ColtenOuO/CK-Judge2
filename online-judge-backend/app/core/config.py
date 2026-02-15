0cb22b8f4b1f8038514038e78b54320a80b34dc9
ColtenOuO <jun930436@gmail.com>
2026-02-15 21:19:00 +0800

    settings: api init

--- online-judge-backend/app/core/config.py
+++ online-judge-backend/app/core/config.py
@@ -9,12 +9,18 @@ class Settings(BaseSettings):
     POSTGRES_PASSWORD: str
     POSTGRES_DB: str
     POSTGRES_HOST: str
+    POSTGRES_PORT: int = 5432
     DATABASE_URL: str
 
     REDIS_URL: str
 
+    SECRET_KEY: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 11520
+    ENV: str = "development"
+
     class Config:
         env_file = ".env"
         case_sensitive = True
+        extra = "ignore"
 
 settings = Settings()
